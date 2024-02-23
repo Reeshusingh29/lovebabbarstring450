74656b1d9537dba3d216978e57af4cd2d58f726b
Reeshu singh <114919870+Reeshusingh29@users.noreply.github.com>
2024-02-23 23:29:27 +0530

    Update defanginganaddress.cpp

--- defanginganaddress.cpp
+++ defanginganaddress.cpp
@@ -3,7 +3,7 @@ public:
     bool checkIfPangram(string sentence) {
         vector<bool>alpha(26,0);
 
-        for(int i=0;i<sentence.size();i++){
+        for(int i=0;i<sentence.size();i++){ 
             alpha[sentence[i]-'a']=1;
         }
         for(int i=0;i<26;i++){
